58307070254d3ede867b5597bf0758dff6916cec
evamats <reon19@mail.ru>
2016-02-09 20:20:49 +0300

    email & other

--- index1.php
+++ index1.php
@@ -1,24 +1,24 @@
-<?php
+﻿<?php
  /* Здесь проверяется существование переменных */
-  if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
- if (isset($_POST['name'])) {$name = $_POST['name'];}
-  if (isset($_POST['addres'])) {$addres = $_POST['addres'];}
+    if (isset($_POST['phone'])) {$phone = $_POST['phone'];}
+    if (isset($_POST['name'])) {$name = $_POST['name'];}
+    if (isset($_POST['addres'])) {$addres = $_POST['addres'];}
 
 /* Сюда впишите свою эл. почту */
- $address = "business.shmidt@yandex.ru";
+$address = "iggywrap@gmail.com";
 
 /* А здесь прописывается текст сообщения, \n - перенос строки */
- $mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name\nE-mail: $addres";
+$mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name\nE-mail: $addres";
 
 /* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ <podbor.ru>'; // от кого
- $send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+$sub='Заказ Eva_DuffCar'; //сабж
+$email='Заказ Eva_DuffCar'; // от кого
+$send = mail ($address,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
 
 ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
+//header('Refresh: 3; URL=index.html');
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!--<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">-->
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -28,15 +28,15 @@ header('Refresh: 3; URL=index.html');
 <style type="text/css">
 body
 {
-   
+
    background: #22BFF7 url(images/zakaz.jpg) top -70% center no-repeat;
-   
+
 }
 
 <script type="text/javascript">
 setTimeout('location.replace("/index.html")', 3000);
 /*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
+</script>
 </head>
 </body>
 </html>
